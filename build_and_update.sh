b291f41fa3320ff49817e8adb734774265e5b28a
zhong.xuan <xuan.zhong@ingka.ikea.com>
2023-12-20 17:37:06 +0400

    modify docker image version

--- build_and_update.sh
+++ build_and_update.sh
@@ -1,3 +1,3 @@
 mvn clean package
-docker build -t flamhaze5946/kline-proxy:1.0.0 --platform linux/amd64 .
-docker push flamhaze5946/kline-proxy:1.0.0
+docker build -t flamhaze5946/kline-proxy:1.1.0 --platform linux/amd64 .
+docker push flamhaze5946/kline-proxy:1.1.0
