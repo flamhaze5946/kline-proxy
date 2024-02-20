7bafe638c9a4c14269d78ae60941703547aec1f9
zhong.xuan <xuan.zhong@ingka.ikea.com>
2024-02-20 17:29:29 +0400

    only print non null field

--- src/main/java/com/zx/quant/klineproxy/config/SerializeConfig.java
+++ src/main/java/com/zx/quant/klineproxy/config/SerializeConfig.java
@@ -1,5 +1,7 @@
 package com.zx.quant.klineproxy.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zx.quant.klineproxy.util.Serializer;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,14 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SerializeConfig {
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.setSerializationInclusion(Include.NON_NULL);
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    return objectMapper;
+  }
 
   @Bean
   public Serializer serializer(ObjectMapper objectMapper) {
