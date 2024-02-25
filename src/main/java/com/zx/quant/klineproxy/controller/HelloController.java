7a571409d263bd1ea2d1452bbfa8ee239a7ea512
zhong.xuan <xuan.zhong@ingka.ikea.com>
2024-02-25 22:49:43 +0400

    add  ip api

--- src/main/java/com/zx/quant/klineproxy/controller/HelloController.java
+++ src/main/java/com/zx/quant/klineproxy/controller/HelloController.java
@@ -1,5 +1,10 @@
 package com.zx.quant.klineproxy.controller;
 
+import com.google.common.collect.Lists;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello")
 public class HelloController {
 
+  private static final List<String> IP_HEADERS = Lists.newArrayList(
+      "X-Forwarded-For", "X-Real-IP", "Forwarded"
+  );
+
   private static final String HELLO_WORLD = "Hello World!";
 
   /**
@@ -21,5 +30,16 @@ public class HelloController {
   @GetMapping("helloWorld")
   public String helloWorld() {
     return HELLO_WORLD;
+  }
+
+  @GetMapping("whatsMyIp")
+  public String whatsMyIp(HttpServletRequest request) {
+    for (String ipHeader : IP_HEADERS) {
+      String ipValue = request.getHeader(ipHeader);
+      if (StringUtils.isNotBlank(ipValue)) {
+        return ipValue;
+      }
+    }
+    return request.getRemoteAddr();
   }
 }
