fe6c89ce7ad1dcacf443f02127410c347b0cef86
zhong.xuan <xuan.zhong@ingka.ikea.com>
2024-07-04 23:44:06 +0400

    exclude redis

--- src/main/java/com/zx/quant/klineproxy/KlineProxyApplication.java
+++ src/main/java/com/zx/quant/klineproxy/KlineProxyApplication.java
@@ -2,8 +2,13 @@ package com.zx.quant.klineproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * kline proxy application
+ * @author flamhaze5946
+ */
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 public class KlineProxyApplication {
 
   public static void main(String[] args) {
