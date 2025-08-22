2264e617c443c24ea7b70354d6941d0d144f9b4f
flamhaze5946 <flamhaze5946@gmail.com>
2025-08-22 13:11:33 +0800

    modify version

--- build_and_update.sh
+++ build_and_update.sh
@@ -1,3 +1,3 @@
 mvn clean package
-docker build -t flamhaze5946/kline-proxy:1.4.6 --platform linux/amd64 .
-docker push flamhaze5946/kline-proxy:1.4.6
+docker build -t flamhaze5946/kline-proxy:1.5.1 --platform linux/amd64 .
+docker push flamhaze5946/kline-proxy:1.5.1
